af91f127e7862bfe973f4773173434ca2ffa76e2
davechai <davecqx@gmail.com>
2020-09-04 18:06:06 +0800

    urql basics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
-
+import { createClient, Provider } from 'urql'
 import theme from '../theme'
+
+const client = createClient({ 
+  url: 'http://localhost:4000/graphql', 
+  fetchOptions: { 
+    credentials: "include"
+  } 
+})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ThemeProvider>
+    <Provider value={client}>
+      <ThemeProvider theme={theme}>
+        <ColorModeProvider>
+          <CSSReset />
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ThemeProvider>
+    </Provider>
   )
 }
 
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,28 +3,36 @@ import { Formik, Form } from "formik"
 import Wrapper from "../components/Wrapper"
 import { InputField } from "../components/InputFields"
 import { Button, Box } from "@chakra-ui/core"
+import { useMutation } from "urql"
 
 interface rigisterProps {
 
 }
 
-const Register: React.FC<rigisterProps> = ({}) => {
-  function validateName(value) {
-    let error
-    if (!value) {
-      error = "Name is required"
-    } else if (value !== "Naruto") {
-      error = "Jeez! You're not a fan 😱"
+const REGISTER_MUT = `
+  mutation($username: String!, $password: String!) {
+    rigister(options: { password: $password, username: $username }) {
+      user {
+        id
+        username
+      }
+      errors {
+        field
+        message
+      }
     }
-    return error
   }
+`
+
+const Register: React.FC<rigisterProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUT)
 
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values)
+        onSubmit={async (values) => {
+          const res = await register(values)
         }}
       >
         {({isSubmitting}) => (
