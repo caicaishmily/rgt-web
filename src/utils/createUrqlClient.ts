cbef0bcdd99ffd6f77b5811072d1531ee08078cd
davechai <davecqx@gmail.com>
2020-10-19 22:52:32 +0800

    write-fragment

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -8,8 +8,10 @@ import {
   MeDocument,
   MeQuery,
   RegisterMutation,
+  VoteMutationVariables,
 } from "../generated/graphql";
 import { betterUpdateQuery } from "./betterUpdateQuery";
+import gql from "graphql-tag";
 
 const errorExchange: Exchange = ({ forward }) => (ops$) => {
   return pipe(
@@ -128,6 +130,32 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          vote: (_result, args, cache, info) => {
+            const { postId, value } = args as VoteMutationVariables;
+
+            const data = cache.readFragment(
+              gql`
+                fragment _ on Post {
+                  id
+                  points
+                }
+              `,
+              {id: postId} as any
+            )
+
+            if (data) {
+              const newPonits = (data.points as number) + value
+
+              cache.writeFragment(
+                gql`
+                  fragment _ on Post {
+                    points
+                  }
+                `,
+                {id: postId, points: newPonits} as any
+              )
+            }
+          },
           createPost: (_result, args, cache, info) => {
             const allFields = cache.inspectFields("Query")
             const fieldInfos = allFields.filter((info) => info.fieldName === "posts")
